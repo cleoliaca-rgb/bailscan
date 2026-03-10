dbe484807edb694c62a87ed984409988bf73160a
cleoliaca-rgb <cleolia.ca@gmail.com>
2026-03-10 18:21:29 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,8 @@
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
   const sig = req.headers['stripe-signature'];
